5342cb5cabab60c00f1841d5a725dbf5e3a0029f
Petar B <el.pionir@gmail.com>
2020-10-21 16:44:06 +0200

    update

--- src/Pages/index.tsx
+++ src/Pages/index.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import {Switch,Route} from "react-router-dom";
 
 const Pages: React.FC = (): JSX.Element=> <Switch>
-    <Route path="/" exact>Home</Route>
+    <Route path="/" exact>Home - Update</Route>
     <Route path="/goal" exact>Goal</Route>
     <Route >Not Found</Route>
 </Switch>
