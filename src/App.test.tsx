2407798589056836c030e75add89e3b19a2e2919
Petar B <el.pionir@gmail.com>
2020-10-21 18:07:26 +0200

    correct test

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,10 @@
 import React from 'react';
 import { render } from '@testing-library/react';
+import { MemoryRouter} from "react-router-dom";
 import App from './App';
 
 test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
+  const { getByText } = render(<MemoryRouter initialEntries={['/']} initialIndex={0} ><App/></MemoryRouter>);
+  const linkElement = getByText(/Goal/i);
   expect(linkElement).toBeInTheDocument();
 });
