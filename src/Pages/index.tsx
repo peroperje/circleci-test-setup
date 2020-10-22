9e2d816646333bc288bcabc7b3588ec4c4cd6ab4
Petar B <el.pionir@gmail.com>
2020-10-22 13:32:21 +0200

    simulation new pull request

--- src/Pages/index.tsx
+++ src/Pages/index.tsx
@@ -2,8 +2,8 @@ import React from 'react';
 import {Switch,Route} from "react-router-dom";
 
 const Pages: React.FC = (): JSX.Element=> <Switch>
-    <Route path="/" exact>Home - Update</Route>
-    <Route path="/goal" exact>Goal - Update - auto build images</Route>
+    <Route path="/" exact>Home - Update -simulation</Route>
+    <Route path="/goal" exact>Goal - Update - auto build images - simulation</Route>
     <Route >Not Found</Route>
 </Switch>
 
